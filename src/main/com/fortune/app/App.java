cf63e1831583c1c01956eba56108b8874d1fa0ed
shilpacvn55 <shilpachavan55555@gmail.com>
2026-01-04 23:53:31 +0530

    Update App.java

--- src/main/com/fortune/app/App.java
+++ src/main/com/fortune/app/App.java
@@ -5,7 +5,7 @@ package com.fortune.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = "Hello World!  happy new year";
 
     public App() {}
 
